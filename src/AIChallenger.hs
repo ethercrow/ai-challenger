395ef57db7c172869862c9d678c7ae0cc3b266b8
Dmitry Ivanov <ethercrow@gmail.com>
2015-07-18 12:14:43 +0600

    Add EKG monitoring

--- src/AIChallenger.hs
+++ src/AIChallenger.hs
@@ -20,6 +20,7 @@ import qualified Data.Text.IO as TIO
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 import Servant
+import qualified System.Remote.Monitoring as EKG
 
 import AIChallenger.Bot
 import AIChallenger.Types
@@ -31,6 +32,7 @@ type WebAPI
 
 startJudge :: Game game => game -> IO ()
 startJudge _game = do
+    EKG.forkServer "localhost" 7999
     stateVar <- newMVar (ServerState mempty)
     Warp.run 8081 (app stateVar)
 
