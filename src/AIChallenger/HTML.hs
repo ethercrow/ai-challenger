bd9258cdc09a40515692091d145b5a5fdfb36388
Dmitry Ivanov <divanov@machinezone.com>
2015-08-18 18:07:26 +0600

    Tourney table

--- src/AIChallenger/HTML.hs
+++ src/AIChallenger/HTML.hs
@@ -8,9 +8,12 @@ module AIChallenger.HTML
     ) where
 
 import Control.Monad (forM_)
+import Data.List (sort)
 import Data.Monoid
 import Lucid
+import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
+import qualified Data.Vector as V
 
 import AIChallenger.Types
 
@@ -26,9 +29,9 @@ instance ToHtml MatchPage where
         doctypehtml_ $ do
             p_ (toHtml ("Match #" <> show mid))
             p_ "Bots:"
-            ul_ $ foldMap (li_ . toHtml . botName) bots
+            ul_ $ foldMap (li_ . botWidget) bots
             p_ "Winners:"
-            ul_ $ foldMap (li_ . toHtml . botName) winners
+            ul_ $ foldMap (li_ . botWidget) winners
             span_ "Game over type: "
             case gameover of
                 Elimination -> "elimination"
@@ -48,6 +51,44 @@ instance ToHtml MainPage where
         doctypehtml_ $ do
             h1_ "Welcome!"
             h2_ "Bots"
-            mapM_ (p_ . toHtml . botName) bots
+            mapM_ (p_ . botWidget) bots
             h2_ "Matches"
-            mapM_ (p_ . toHtml . show) matches
+            table_ $ do
+                tr_ $ do
+                    th_ ""
+                    foldMap (th_ . botWidget) bots
+                V.forM_ bots $ \b1 -> tr_ $ do
+                    td_ (botWidget b1)
+                    V.forM_ bots $ \b2 -> do
+                        let predicate = (== (sort [b1, b2])) . sort . V.toList . matchBots
+                        case filter predicate (V.toList matches) of
+                            match : _ -> td_ (matchWidget match b1)
+                            [] -> td_ ""
+
+newtype BotWidget = BotWidget Bot
+
+instance ToHtml BotWidget where
+    toHtml (BotWidget (Bot name (ExecutableBot exe))) =
+        span_ [title_ (T.pack (show exe))] (toHtml name)
+    toHtml (BotWidget (Bot name _)) = toHtml name
+
+botWidget :: Monad m => Bot -> HtmlT m ()
+botWidget = toHtml . BotWidget
+
+data MatchWidget = MatchWidget Match Bot
+
+instance ToHtml MatchWidget where
+    toHtml (MatchWidget (Match (MatchId mid) _ winners gameover _) b1) =
+        let text = winText <> ":" <> gameoverText
+            winText =
+                if winners == pure b1 then "W"
+                else if V.length winners == 1 then "L"
+                else "D"
+            gameoverText = case gameover of
+                Disqualification _ -> "DQ"
+                TurnLimit -> "TL"
+                Elimination -> "E"
+        in a_ [href_ ("match/" <> T.pack (show mid))] text
+
+matchWidget :: Monad m => Match -> Bot -> HtmlT m ()
+matchWidget match bot = toHtml (MatchWidget match bot)
