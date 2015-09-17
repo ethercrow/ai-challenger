e2fa6a562458088c10b17a448047cb3e45aacd3d
Dmitry Ivanov <ethercrow@gmail.com>
2015-09-18 01:29:34 +0600

    Add nodejs to shell.nix for building dashboard

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ with pkgs;
 stdenv.mkDerivation {
   name = "ai-challenger";
 
-  buildInputs = [ haskellPackages.stack haskell.compiler.ghc7102 zlib ];
+  buildInputs = [ haskellPackages.stack haskell.compiler.ghc7102 zlib nodejs ];
 
   shellHook = ''
     export LD_LIBRARY_PATH="${zlib}/lib:$LD_LIBRARY_PATH"
